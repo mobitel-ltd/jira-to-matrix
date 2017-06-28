b4e753396ee942014c734ae4cc4059ed69894861
mleg <@>
2017-06-28 12:56:52 +0300

    Fix en locale

--- src/locales/en.js
+++ src/locales/en.js
@@ -4,6 +4,12 @@ const dict = Object.freeze({
     comment_updated: 'changed comment',
     issue_updated: 'changed issue',
     issueHasChanged: 'Task was changed',
+    statusHasChanged: '%{issue.key} "%{issue.fields.summary}" now has status "%{status}"',
+    statusHasChangedMessage: '%{user.name} changed a linked issue status [%{issue.key} "%{issue.fields.summary}"](%{issue.ref}) to **%{status}**',
+    newIssueInEpic: 'New issue in epic',
+    issueAddedToEpic: 'An issue [%{issue.key} %{issue.fields.summary}](%{issue.ref}) was added to the epic',
+    newLink: 'New link',
+    newLinkMessage: 'A new link. This issue **%{relation}** [%{key} "%{summary}"](%{ref})',
 })
 
 module.exports.dict = dict
