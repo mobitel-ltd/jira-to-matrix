0e21682f18112dda4cfbaf99bd6a731df8038c98
disko23 <ieee@mail.ru>
2017-08-22 10:43:18 +0300

    BBCOM-884 Попытка исправить потерю хуков

--- src/bot/connect-to-matrix.js
+++ src/bot/connect-to-matrix.js
@@ -1,11 +1,25 @@
 const matrix = require('../matrix')
+const logger = require('simple-color-logger')()
 
 async function middleware(req, res, next) {
-    const client = await matrix.connect()
-    if (!client) {
-        next(new Error('Could not connect to Matrix'))
-        return
+    let client = await matrix.connect()
+    let boolErr = false
+    let count = 0
+
+    while (!client) {
+        client = matrix.connect()
+        boolErr = true
+
+        if (count >= 10) {
+            next(new Error('Could not connect to Matrix'))
+        }
+        count += 1
     }
+
+    if (boolErr) {
+        logger.error('Could not connect to Matrix')
+    }
+
     req.mclient = client
     next()
 }
