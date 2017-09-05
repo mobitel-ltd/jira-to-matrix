0d46e08d34a2e3eb703292488fd2a8be08ed9075
disko23 <ieee@mail.ru>
2017-09-05 18:17:00 +0300

    fix log response jira

--- src/utils/rest.js
+++ src/utils/rest.js
@@ -13,12 +13,7 @@ async function fetchJSON(url/*:string*/, basicAuth/*:string*/) {
         logger.error(`Error while getting ${url}:\n${err}`)
         return undefined
     }
-    logger.info(response._convert().toString())
-    try {
-        logger.info(JSON.parse(response._convert().toString()))
-    } catch (err) {
-        logger.error(err.message)
-    }
+    logger.info(`response from jira have status: ${response.status}`)
     const [parseErr, object] = await to(response.json())
     if (parseErr) {
         logger.error(`Error while parsing JSON from ${url}:\n${parseErr}`)
