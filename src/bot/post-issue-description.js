82b5ff6b0d93b9bdb377c2aae3726963c4a6ee6f
disko23 <ieee@mail.ru>
2017-09-08 11:51:24 +0300

    fix invalid epic name

--- src/bot/post-issue-description.js
+++ src/bot/post-issue-description.js
@@ -1,15 +1,12 @@
 const _ = require('lodash');
 const htmlToText = require('html-to-text').fromString;
 const jira = require('../jira');
+const logger = require('simple-color-logger')();
 
 function getTextIssue(req, address) {
     const text = String(
-        _.get(req.body.issue.fields, address) || ''
+        _.get(req.body.issue.fields, address) || 'отсутствует'
     ).trim();
-
-    if (!text) {
-        return 'отсутствует';
-    }
 
     return text;
 }
@@ -22,31 +19,47 @@ async function getPost(req) {
     const epicLink = getTextIssue(req, 'customfield_10006');
     const estimateTime = getTextIssue(req, 'reporter.timeestimate');
     const description = getTextIssue(req, 'description');
+    let post;
 
-    const epick = await jira.issue.getFormatted(epicLink);
-    let nameEpick;
-    if (typeof epick === 'object' && epick.key === epicLink) {
-        nameEpick = String(_.get(epick.renderedField, 'customfield_10005') 
-            || _.get(epick.fields, 'customfield_10005') 
-            || epicLink);
+    if (epicLink !== 'отсутствует') {
+        const epic = await jira.issue.getFormatted(epicLink);
+        let nameEpic;
+        if (typeof epic === 'object' && epic.key === epicLink) {
+            nameEpic = String(_.get(epic.renderedField, 'customfield_10005') 
+                || _.get(epic.fields, 'customfield_10005') 
+                || epicLink
+            );
+        }
+
+        logger.info(`Epic name: ${nameEpic}; epic key: ${epicLink}`);
+
+        post = `
+            Assignee: 
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;${assigneeName}
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;${assigneeEmail}<br>
+            <br>Reporter: 
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;${reporterName}
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;${reporterEmail}<br>
+            <br>Epic link: 
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;${nameEpic} (${epicLink})
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;\thttps://jira.bingo-boom.ru/jira/browse/${epicLink}<br>
+            <br>Estimate time: 
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;${estimateTime}<br>
+            <br>Description: 
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;${description}<br>`;
+    } else {
+        post = `
+            Assignee: 
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;${assigneeName}
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;${assigneeEmail}<br>
+            <br>Reporter: 
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;${reporterName}
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;${reporterEmail}<br>
+            <br>Estimate time: 
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;${estimateTime}<br>
+            <br>Description: 
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;${description}<br>`;
     }
-
-    const post = `
-        Assignee: 
-            <br>&nbsp;&nbsp;&nbsp;&nbsp;${assigneeName}
-            <br>&nbsp;&nbsp;&nbsp;&nbsp;${assigneeEmail}<br>
-        <br>Reporter: 
-            <br>&nbsp;&nbsp;&nbsp;&nbsp;${reporterName}
-            <br>&nbsp;&nbsp;&nbsp;&nbsp;${reporterEmail}<br>
-        <br>Epic link: 
-        <br>&nbsp;&nbsp;&nbsp;&nbsp;${nameEpick} (${epicLink})
-            <br>&nbsp;&nbsp;&nbsp;&nbsp;\thttps://jira.bingo-boom.ru/jira/browse/${epicLink}<br>
-        <br>Estimate time: 
-            <br>&nbsp;&nbsp;&nbsp;&nbsp;${estimateTime}<br>
-        <br>Description: 
-            <br>&nbsp;&nbsp;&nbsp;&nbsp;${description}<br>
-        `;
-
     return post;
 }
 
