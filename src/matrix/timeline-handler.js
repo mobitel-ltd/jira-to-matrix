e51104205adb91ed48b1225b23509bdb8a56e396
disko23 <ieee@mail.ru>
2017-10-02 16:01:16 +0300

    add handler events from matrix

--- src/matrix/timeline-handler.js
+++ src/matrix/timeline-handler.js
@@ -1,4 +1,5 @@
 const fetchPostJSON = require('../utils').fetchPostJSON;
+const fetchJSON = require('../utils').fetchJSON;
 
 module.exports = async function(event, room, toStartOfTimeline) {
     if (toStartOfTimeline) {
@@ -7,20 +8,37 @@ module.exports = async function(event, room, toStartOfTimeline) {
     if (event.getType() !== "m.room.message") {
         return;
     }
+
+    const command = await eventFromMatrix(event, room);
+    console.log(command);
+}
+
+const eventFromMatrix = async (event, room) => {
     const body = event.getContent().body;
+    const op = body.match(/!\w*\b/g);
 
-    if (body.match(/!comment/ig)) {
-        const message = body.split(/!comment/ig).join(' ');
-        let sender = event.getSender();
-        sender = sender.substring(1, sender.length - 21);
-        let roomName = room.getCanonicalAlias();
-        roomName = roomName.substring(1, roomName.length - 21);
+    if (!op) {
+        return;
+    }
 
-        let jiraR = await fetchPostJSON(
-            `https://jira.bingo-boom.ru/jira/rest/api/2/issue/${roomName}/comment`,
-            'Basic amlyYV90ZXN0X2JvdDp4TDFCSTNDaFcyZGI3Tg==',
-            schema(sender, message)
-        );
+    switch (op[0]) {
+        case '!comment':
+            const message = body.split(/!comment/i).join(' ');
+            let sender = event.getSender();
+            sender = sender.substring(1, sender.length - 21);
+            let roomName = room.getCanonicalAlias();
+            roomName = roomName.substring(1, roomName.length - 21);
+    
+            let jiraR = await fetchPostJSON(
+                `https://jira.bingo-boom.ru/jira/rest/api/2/issue/${roomName}/comment`,
+                'Basic amlyYV90ZXN0X2JvdDp4TDFCSTNDaFcyZGI3Tg==',
+                schema(sender, message)
+            );
+            return 'comment to jira';
+        case '!move':
+            return 'move issue';
+        default:
+            return;
     }
 }
 
@@ -30,3 +48,7 @@ const schema = (sender, message) => {
         "body": post
     });
 }
+
+// const schemaMove = () => {
+//     return JSON.stringify("aa_makarov");
+// }
