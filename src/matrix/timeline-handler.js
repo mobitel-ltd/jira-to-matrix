b37d30ce4b6528ea386324ebeba3efb461a00667
disko23 <ieee@mail.ru>
2017-10-02 18:52:42 +0300

    add assign command

--- src/matrix/timeline-handler.js
+++ src/matrix/timeline-handler.js
@@ -1,5 +1,6 @@
-const fetchPostJSON = require('../utils').fetchPostJSON;
-const fetchJSON = require('../utils').fetchJSON;
+const jiraRequest = require('../utils');
+const {auth} = require('../jira');
+const logger = require('simple-color-logger')();
 
 module.exports = async function(event, room, toStartOfTimeline) {
     if (event.getType() !== "m.room.message" || toStartOfTimeline) {
@@ -7,6 +8,11 @@ module.exports = async function(event, room, toStartOfTimeline) {
     }
 
     const command = await eventFromMatrix(event, room);
+    if (command) {
+        logger.info(command);
+    }
+
+    return;
 }
 
 const eventFromMatrix = async (event, room) => {
@@ -17,23 +23,37 @@ const eventFromMatrix = async (event, room) => {
         return;
     }
 
+    let roomName = room.getCanonicalAlias();
+    roomName = roomName.substring(1, roomName.length - 21);
+    let sender = event.getSender();
+    sender = sender.substring(1, sender.length - 21);
+
     switch (op[0]) {
         case '!comment':
             const message = body.split(/!comment/i).join(' ');
-            let sender = event.getSender();
-            sender = sender.substring(1, sender.length - 21);
-            let roomName = room.getCanonicalAlias();
-            roomName = roomName.substring(1, roomName.length - 21);
     
-            let jiraR = await fetchPostJSON(
+            const jiraComment = await jiraRequest.fetchPostJSON(
                 `https://jira.bingo-boom.ru/jira/rest/api/2/issue/${roomName}/comment`,
-                'Basic amlyYV90ZXN0X2JvdDp4TDFCSTNDaFcyZGI3Tg==',
+                auth(),
                 schema(sender, message)
             );
-            return 'comment to jira';
-        case '!move':
-            return 'move issue';
+
+            return `Comment from ${sender} for ${roomName}`;
+        case '!assign':
+            const assignee = getAssgnee(event);
+
+            const jiraAssign = await jiraRequest.fetchPutJSON(
+                `https://jira.bingo-boom.ru/jira/rest/api/2/issue/${roomName}/assignee`,
+                auth(),
+                schemaAssignee(assignee)
+            );
+
+            if (jiraAssign.status !== 204) {
+                return `User ${assignee} or room ${roomName} don't exist`;
+            }
+            return `The user ${assignee} now assignee issue ${roomName}\n(did ${sender})`;
         default:
+            logger.warn(`The command ${op[0]} failed`);
             return;
     }
 }
@@ -45,6 +65,18 @@ const schema = (sender, message) => {
     });
 }
 
-// const schemaMove = () => {
-//     return JSON.stringify("aa_makarov");
-// }
+const schemaAssignee = (assignee) => {
+    return JSON.stringify({
+        "name": `${assignee}`
+    });
+}
+
+const getAssgnee = (event) => {
+    const body = event.getContent().body;
+    if (body === '!assign') {
+        const sender = event.getSender();
+        return sender.substring(1, sender.length - 21);
+    }
+
+    return body.substring(8, body.length);
+}
