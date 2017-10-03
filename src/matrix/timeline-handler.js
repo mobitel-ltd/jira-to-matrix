d7b5c811e3cabbd41c128b3ff0ad067cc78d0d8f
disko23 <ieee@mail.ru>
2017-10-03 11:14:10 +0300

    timeline-handler refactoring

--- src/matrix/timeline-handler.js
+++ src/matrix/timeline-handler.js
@@ -8,11 +8,18 @@ const handler =  async function(event, room, toStartOfTimeline) {
         return;
     }
     const self = this;
+    
+    // postfix charsets in matrix names
+    // matrix sends "@jira_test:matrix.bingo-boom.ru"
+    // but i need only "jira_test"
+    const postfix = 21;
+    let sender = event.getSender();
+    sender = sender.substring(1, sender.length - postfix);
 
     try {
-        const command = await eventFromMatrix(event, room, self);
+        const command = await eventFromMatrix(event, room, sender, self);
         if (command) {
-            logger.info(command);
+            logger.info(`${command}\n(did ${sender})`);
         }
 
         return;
@@ -24,7 +31,7 @@ const handler =  async function(event, room, toStartOfTimeline) {
     }
 }
 
-const eventFromMatrix = async (event, room, self) => {
+const eventFromMatrix = async (event, room, sender, self) => {
     const body = event.getContent().body;
     const op = body.match(/!\w*\b/g);
 
@@ -38,44 +45,62 @@ const eventFromMatrix = async (event, room, self) => {
     const postfix = 21;
     let roomName = room.getCanonicalAlias();
     roomName = roomName.substring(1, roomName.length - postfix);
-    let sender = event.getSender();
-    sender = sender.substring(1, sender.length - postfix);
 
     switch (op[0]) {
         case '!comment':
-            const message = body.split(/!comment/i).join(' ');
-
-            // post comment in issue
-            const jiraComment = await jiraRequest.fetchPostJSON(
-                `https://jira.bingo-boom.ru/jira/rest/api/2/issue/${roomName}/comment`,
-                auth(),
-                schemaComment(sender, message)
-            );
-
-            return `Comment from ${sender} for ${roomName}`;
+            return await postComment(body, sender, roomName);
         case '!assign':
-            const assignee = getAssgnee(event);
-
-            // appointed assignee for issue
-            const jiraAssign = await jiraRequest.fetchPutJSON(
-                `https://jira.bingo-boom.ru/jira/rest/api/2/issue/${roomName}/assignee`,
-                auth(),
-                schemaAssignee(assignee)
-            );
-
-            if (jiraAssign.status !== 204) {
-                const post = t('errorMatrixAssign', {assignee});
-                await self.sendHtmlMessage(room.roomId, post, post);
-                return `User ${assignee} or room ${roomName} don't exist`;
-            } 
-
-            const post = t('successMatrixAssign', {assignee});
-            await self.sendHtmlMessage(room.roomId, post, post);
-            return `The user ${assignee} now assignee issue ${roomName}\n(did ${sender})`;
+            return await appointAssignee(event, room, roomName, self);
         default:
             logger.warn(`The command ${op[0]} failed`);
             return;
     }
+}
+
+const postComment = async (body, sender, roomName) => {
+    const message = body.split(/!comment/i).join(' ');
+    
+        // post comment in issue
+        const jiraComment = await jiraRequest.fetchPostJSON(
+            `https://jira.bingo-boom.ru/jira/rest/api/2/issue/${roomName}/comment`,
+            auth(),
+            schemaComment(sender, message)
+        );
+
+        return `Comment from ${sender} for ${roomName}`;
+}
+
+const appointAssignee = async (event, room, roomName, self) => {
+    const assignee = getAssgnee(event);
+    
+    // appointed assignee for issue
+    const jiraAssign = await jiraRequest.fetchPutJSON(
+        `https://jira.bingo-boom.ru/jira/rest/api/2/issue/${roomName}/assignee`,
+        auth(),
+        schemaAssignee(assignee)
+    );
+
+    if (jiraAssign.status !== 204) {
+        const post = t('errorMatrixAssign', {assignee});
+        await self.sendHtmlMessage(room.roomId, post, post);
+        return `User ${assignee} or room ${roomName} don't exist`;
+    } 
+
+    const post = t('successMatrixAssign', {assignee});
+    await self.sendHtmlMessage(room.roomId, post, post);
+    return `The user ${assignee} now assignee issue ${roomName}`;
+}
+
+const getAssgnee = (event) => {
+    const body = event.getContent().body;
+    const postfix = 21;
+    if (body === '!assign') {
+        const sender = event.getSender();
+        return sender.substring(1, sender.length - postfix);
+    }
+
+    // 8 it's length command "!assign"
+    return body.substring(8, body.length);
 }
 
 const schemaComment = (sender, message) => {
@@ -89,18 +114,6 @@ const schemaAssignee = (assignee) => {
     return JSON.stringify({
         "name": `${assignee}`
     });
-}
-
-const getAssgnee = (event) => {
-    const body = event.getContent().body;
-    const postfix = 21;
-    if (body === '!assign') {
-        const sender = event.getSender();
-        return sender.substring(1, sender.length - postfix);
-    }
-
-    // 8 it's length command "!assign"
-    return body.substring(8, body.length);
 }
 
 module.exports = handler;
