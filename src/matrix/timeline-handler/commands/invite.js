5cb67c6b7847b6cc373573d89bed5cb46bae8446
disko23 <ieee@mail.ru>
2017-10-23 17:27:25 +0300

    combed invite.js

--- src/matrix/timeline-handler/commands/invite.js
+++ src/matrix/timeline-handler/commands/invite.js
@@ -1,25 +1,25 @@
 /* eslint-disable camelcase */
 const {domain, admins} = require('../../../config').matrix;
 
-const getRoomId = async (body, self) => {
+const getRoomId = async (body, matrixClient) => {
     const room = body.substring(8).trim();
     if (~room.indexOf(domain)) {
-        const {room_id} = await self.getRoomIdForAlias(room);
+        const {room_id} = await matrixClient.getRoomIdForAlias(room);
         return room_id;
     }
 
     const alias = `#${room.toUpperCase()}:${domain}`;
-    const {room_id} = await self.getRoomIdForAlias(alias);
+    const {room_id} = await matrixClient.getRoomIdForAlias(alias);
 
     return room_id;
 };
 
-module.exports = async ({body, sender, self}) => {
+module.exports = async ({body, sender, matrixClient}) => {
     if (!admins.includes(sender)) {
         return;
     }
 
-    const roomId = await getRoomId(body, self);
+    const roomId = await getRoomId(body, matrixClient);
     const userId = `@${sender}:${domain}`;
-    await self.invite(roomId, userId);
+    await matrixClient.invite(roomId, userId);
 };
