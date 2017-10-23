d5ab32031aea042b862a5d07dddf8beb86840811
disko23 <ieee@mail.ru>
2017-10-23 17:56:49 +0300

    combed comment.js

--- src/matrix/timeline-handler/commands/comment.js
+++ src/matrix/timeline-handler/commands/comment.js
@@ -4,26 +4,26 @@ const translate = require('../../../locales');
 const {schemaComment} = require('./schemas.js');
 const {BASE_URL} = require('./helper.js');
 
-module.exports = async ({body, sender, room, roomName, self}) => {
+module.exports = async ({body, sender, room, roomName, matrixClient}) => {
     const message = body.substring(9).trim();
 
     // post comment in issue
-    const jiraComment = await jiraRequest.fetchPostJSON(
+    const {status} = await jiraRequest.fetchPostJSON(
         `${BASE_URL}/${roomName}/comment`,
         auth(),
         schemaComment(sender, message)
     );
 
-    if (jiraComment.status !== 201) {
+    if (status !== 201) {
         const post = translate('errorMatrixComment');
-        await self.sendHtmlMessage(room.roomId, post, post);
+        await matrixClient.sendHtmlMessage(room.roomId, post, post);
         return `
             Comment from ${sender} for ${roomName} not published
-            \nJira have status ${jiraComment.status}
+            \nJira have status ${status}
         `;
     }
 
     const post = translate('successMatrixComment');
-    await self.sendHtmlMessage(room.roomId, post, post);
+    await matrixClient.sendHtmlMessage(room.roomId, post, post);
     return `Comment from ${sender} for ${roomName}`;
 };
