16d2159bcdadf6d2db7180530a3687cb6163d0de
Grigori Grudinin <grgr14@mail.ru>
2017-11-29 14:04:31 +0300

    get all users update

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/newline-after-import */
-const rest = require('./rest');
+const rest = require('./rest.js');
+const checkNodeVersion = require('./check-node-version.js');
+const fp = require('./fp.js');
 
-module.exports.fetchJSON = rest.fetchJSON;
-module.exports.fetchPostJSON = rest.fetchPostJSON;
-module.exports.fetchPutJSON = rest.fetchPutJSON;
-module.exports.paramsToQueryString = rest.paramsToQueryString;
-module.exports.checkNodeVersion = require('./check-node-version');
-module.exports.fp = require('./fp');
+module.exports = {
+    ...rest,
+    checkNodeVersion,
+    fp,
+};
