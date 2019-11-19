98fabc8a9cce2228775566a30bf55e6e84903e1d
mixassio <mixail.bondarev@gmail.com>
2019-11-19 12:43:43 +0300

    fix alg

--- src/bot/actions/invite-new-members.js
+++ src/bot/actions/invite-new-members.js
@@ -12,26 +12,20 @@ module.exports = async ({ chatApi, issue }) => {
         const issueWatchers = await getIssueWatchers(issue);
         const issueWatchersChatIds = issueWatchers.map(user => chatApi.getChatUserId(user));
 
-        const newMembers = Ramda.difference(issueWatchersChatIds, chatRoomMembers).filter(Boolean);
-
         const {
             messenger: { bots },
         } = conf;
 
-        const newMembersBot = Ramda.intersection(bots, newMembers);
-        const botsInRoom = Ramda.intersection(bots, chatRoomMembers);
-
-        const membersForInvite =
-            newMembersBot.length && botsInRoom.length ? Ramda.difference(newMembers, bots) : newMembers;
+        const newMembers = Ramda.difference(issueWatchersChatIds, [...chatRoomMembers, ...bots]).filter(Boolean);
 
         await Promise.all(
-            membersForInvite.map(async userID => {
+            newMembers.map(async userID => {
                 await chatApi.invite(roomId, userID);
                 logger.debug(`New member ${userID} invited to ${issue.key}`);
             }),
         );
 
-        return membersForInvite;
+        return newMembers;
     } catch (err) {
         throw errorTracing('inviteNewMembers', err);
     }
